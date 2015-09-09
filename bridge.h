944c818e9dcb9bb4c27adf6fc3030e299d3d3de6
VincentDay <vincent.day@gmail.com>
2015-09-09 13:11:34 -0400

    AFNetworking integrated

---
+++ bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  WebServicesExample
+//
+//  Created by Vince Day on 9/9/15.
+//  Copyright (c) 2015 Vince Day. All rights reserved.
+//
+
+#ifndef WebServicesExample_bridge_h
+#define WebServicesExample_bridge_h
+#import "AFNetworking/AFNetworking.h"
+
+
+#endif

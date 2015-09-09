6c1f52da8d79ddd71b883b7ddb24f20a798ede37
VincentDay <vincent.day@gmail.com>
2015-09-09 17:24:09 -0400

    WebServices Call

--- WebServicesExample/ViewController.swift
+++ WebServicesExample/ViewController.swift
@@ -13,6 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let manager = AFHTTPRequestOperationManager()
+        manager.GET( "http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1",
+            parameters: nil,
+            success:
+            { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                println("Response: " + responseObject.description)
+            },
+            failure:
+            {
+                (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+            })
     }
 
     override func didReceiveMemoryWarning() {
